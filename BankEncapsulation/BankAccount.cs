7716c984db15fdbdf89181891504413fde3133d4
TH8359 <hogue73@embarqmail.com>
2025-11-19 13:14:35 -0500

    Made a class that lets the user deposit money and view the current balance

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,16 @@
+namespace BankEncapsulation;
+
+public class BankAccount
+{
+    private double _balance = 0;
+
+    public void Deposit(double newBalance)
+    {
+        _balance = newBalance;
+    }
+
+    public double GetBalance()
+    {
+        return _balance;
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             double currentBalance = 0;
+            
+            
+            BankAccount andy = new BankAccount();
+            
+            void GetCurrentBalance()
+            {
+                currentBalance = andy.GetBalance();
+                Console.WriteLine($"Your current balance is ${currentBalance}.");
+            }
+            
+            Console.WriteLine("Welcome! How much would you like to deposit today?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+            
+            andy.Deposit(amountToDeposit);
+            
+            Console.WriteLine("If you would like to view your current balance, type \"YES\". Otherwise, type \"NO\".");
+            string response = Console.ReadLine();
+
+            if (response == "YES")
+            {
+                GetCurrentBalance(); 
+            }
+            else if (response == "NO")
+            {
+                Console.WriteLine("Thank you. Goodbye.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+            
+            
+            
+            
         }
     }
 }
